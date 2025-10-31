ef44d4df9a0faba41a42ec1c12e71e1efcfc06ef
Tharun-Saravanan <s.tharun143.123@gmail.com>
2025-10-31 10:39:19 +0530

    Implement hash routing for static hosting compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { AuthProvider } from './contexts/AuthContext'
